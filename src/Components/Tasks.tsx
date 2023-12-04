219ae719cce6d2b4cabb8c993f5532c3c1d9edf3
TenScoops <p.ayres9@gmail.com>
2023-12-04 13:08:50 -0500

    new changes

--- src/Components/Tasks.tsx
+++ src/Components/Tasks.tsx
@@ -24,10 +24,10 @@ const Tasks = () => {
             </div>
             
             {/* Fitness */}
-            <div className=" ">
+            <div className=" hover:translate-y-[-5px] transition-transform duration-200  ">
                 <div className="flex justify-between"><h1 className="text-normal mb-2">Fitness</h1> <button><CiSquarePlus size={25} className="icon-hover"/></button>
 </div>
-                <div className="flex justify-between w-96 h-32 border custom-gray border-black shadow-sharp  cursor-pointer hover:translate-y-[-5px] transition-transform duration-200 cursor-pointer" >
+                <div className="flex justify-between w-96 h-32 border custom-gray border-black shadow-sharp cursor-pointer" >
                     <div className="ml-6 mt-4 space-y-4">
                         <form className="flex items-center">
                             <input type="checkbox" name="category" id="fitness" className="custom-checkbox form-radio h-5 w-5 rounded-none text-gray-600" />
@@ -43,8 +43,8 @@ const Tasks = () => {
                             
                         </form>
                     </div>
-                    <div className=" flex items-end mr-2">
-                        <IoIosCheckbox className="hover:text-gray-700"  size ={30}/>
+                    <div className=" flex items-end mr-1 mb-1">
+                        <IoIosCheckbox className="hover:text-gray-700"  size ={35}/>
                     </div>
                 </div>
             </div>
