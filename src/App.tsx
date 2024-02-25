97ad4d60dc08038722856b8093a44e7c8817d1c0
Unomars4 <une.nyide@gmail.com>
2024-02-25 23:16:56 +0200

    add axios and axios types

--- src/App.tsx
+++ src/App.tsx
@@ -10,42 +10,42 @@ import './index.css'
 import {Route, Routes, } from "react-router-dom"
 
 
-const App = ()=> {
+const App = () => {
 
-  // const handleLoginSuccess = () => {
-  //   return <Navigate to="/" />;
-  // }
-  return (
-      <div >
-        {/* landing page */}
-        <div className=" bg-white font-source-code-pro tracking-tight">
-          <Routes>
-                <Route path="/landing" element={<LandingPage />}/>
-          </Routes>
-        </div>
-        {/* login/register */}
-        <div>
-          <Routes></Routes>
-        </div>
+    // const handleLoginSuccess = () => {
+    //   return <Navigate to="/" />;
+    // }
+    return (
+        <div >
+            {/* landing page */}
+            <div className=" bg-white font-source-code-pro tracking-tight">
+                <Routes>
+                    <Route path="/landing" element={<LandingPage />} />
+                </Routes>
+            </div>
+            {/* login/register */}
+            <div>
+                <Routes></Routes>
+            </div>
 
-        {/* main content */}
-        <div className=" bg-white font-source-code-pro">
-        
-          <Navbar />
+            {/* main content */}
+            <div className=" bg-white font-source-code-pro">
 
-          <Routes>
-              {/* Webapp nav links */}
-              <Route path="/" element={<Home />} />
-              <Route path="/quests" element={<Quests />} />
-              <Route path="/checkin" element={<CheckinParent />} />
-              <Route path="/skilltree" element={<Welcomepage />} /> 
-              {/* Link for authentication */}
-              {/* <Route path="/login/success" element={handleLoginSuccess()} />    */}
-          </ Routes>
-          
+                <Navbar />
+
+                <Routes>
+                    {/* Webapp nav links */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="/quests" element={<Quests />} />
+                    <Route path="/checkin" element={<CheckinParent />} />
+                    <Route path="/skilltree" element={<Welcomepage />} />
+                    {/* Link for authentication */}
+                    {/* <Route path="/login/success" element={handleLoginSuccess()} />    */}
+                </ Routes>
+
+            </div>
         </div>
-      </div>
-  )
+    )
 }
 
 export default App
